f4b5bc241a7bb95ce2e41122d2483d7fa0602688
Craig Gomez <craigacgomez@gmail.com>
2015-04-07 10:24:36 -0700

    Tune facelock properties

--- device.mk
+++ device.mk
@@ -239,12 +239,12 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 # Facelock properties
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.facelock.black_timeout=700 \
+    ro.facelock.black_timeout=400 \
     ro.facelock.det_timeout=1500 \
     ro.facelock.rec_timeout=2500 \
     ro.facelock.lively_timeout=2500 \
-    ro.facelock.est_max_time=500 \
-    ro.facelock.use_intro_anim=true \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false
 
 # Media properties
 PRODUCT_PROPERTY_OVERRIDES := \
