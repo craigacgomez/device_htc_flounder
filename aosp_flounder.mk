0da1a0dcfd30ba4da607340d3dfa4cf86457aed8
Craig Gomez <craigacgomez@gmail.com>
2015-11-01 16:46:21 -0800

    Set correct values for product definition properties

--- aosp_flounder.mk
+++ aosp_flounder.mk
@@ -33,7 +33,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 PRODUCT_NAME := aosp_flounder
 PRODUCT_DEVICE := flounder
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Flounder
-PRODUCT_MANUFACTURER := HTC
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 9
+PRODUCT_MANUFACTURER := htc
 PRODUCT_RESTRICT_VENDOR_FILES := owner path
